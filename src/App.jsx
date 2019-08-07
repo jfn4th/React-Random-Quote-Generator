b5c414d5398e1d84ab05686ba91825fb72c954f9
Jamie Nelson <jfn4th@gmail.com>
2019-08-07 14:06:36 -0500

    add random quote api and quote on startup

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
 import React from 'react';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends React.Component {
 	constructor(props) {
 		super(props);
@@ -32,10 +11,36 @@ class App extends React.Component {
 		};
 	}
 
+	componentDidMount() {
+		fetch('https://api.quotable.io/random').then((results) => results.json()).then((data) => {
+			let quote = data.content;
+			let author = data.author;
+			this.setState({
+				quote: quote,
+				author: author
+			});
+		});
+	}
+
 	render() {
 		return (
-			<div className="App">
-				<h1>This is rendered by react!</h1>
+			<div id="wrapper">
+				<div id="quote-box">
+					<div id="text-area">
+						<span id="text">{this.state.quote}</span>
+					</div>
+					<div id="author-area">
+						<span id="author">{this.state.author}</span>
+					</div>
+					<div id="button-area">
+						<div id="share-btns">
+							<a href="" id="tweet-quote" />
+						</div>
+						<div id="new-btn">
+							<button id="new-quote" />
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
